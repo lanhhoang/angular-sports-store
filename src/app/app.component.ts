84f842925fb3513dbb30ad049197d9d5311e04eb
Lanh Hoang <lanhhoang@users.noreply.github.com>
2022-11-08 18:22:15 -0500

    Updating the Root Component and Root Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `<div class="bg-success p-2 text-center text-white">
-    This is SportsStore
-  </div>`,
+  selector: 'app',
+  template: '<store></store>',
 })
 export class AppComponent {
   title = 'SportsStore';
